59125d107ce8f6b2ba77c96837237db124cf574e
lukeparham <lappp9@gmail.com>
2012-02-11 21:46:23 -0600

    gems

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,20 @@
 (function($){	
 	$(function(){
-		$('.carousel').carousel()
+		$('.carousel').carousel({})
+		$('.carousel').on('slide',function(){
+			$activePill = $(".nav-pills .active");
+			var number;
+			for(number = 1; number<=4; number++){
+				if($activePill.hasClass(number)){
+					next = number +1;
+					if(number === 4){
+						next = 1;
+					}
+					$activePill.removeClass("active");
+					$(".nav-pills ."+next).addClass("active");
+				}
+			}
+		});
 	});
+	
 })(jQuery);
